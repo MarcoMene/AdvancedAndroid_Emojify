7c9f40e2eee907030adc74e47658a1480e9eb76b
Marco Meneghelli <mm@bendingspoons.com>
2017-09-10 15:03:12 +0200

    mm solution (tested on Sabri phone)

---
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,43 @@
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.util.SparseArray;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+/**
+ * Created by marcomeneghelli on 9/10/17.
+ */
+
+public class Emojifier {
+
+    public static void detectFaces(Context context, Bitmap bitmap) {
+
+        // detect faces + log number of
+        FaceDetector detector = new FaceDetector.Builder(context)
+                .setTrackingEnabled(false)
+                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .build();
+
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+
+        SparseArray<Face> faces = detector.detect(frame);
+
+        int nFaces = faces.size();
+
+        Log.i("Emojifier", nFaces + " faces detected in this pic.");
+        Toast.makeText(context, nFaces + " faces detected in this pic.", Toast.LENGTH_SHORT).show();
+
+        // if no faces --> Toast
+        if (nFaces < 1) {
+            Toast.makeText(context, "No faces detected in this pic.", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+}
